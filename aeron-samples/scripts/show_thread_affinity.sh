3f82c13ab2033297910a6b0655265f0fde59d31b
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2025-05-02 12:09:51 +0200

    [Bash] Simplify thread affinity listing.

--- aeron-samples/scripts/show_thread_affinity.sh
+++ aeron-samples/scripts/show_thread_affinity.sh
@@ -4,14 +4,7 @@ set -euo pipefail
 
 pname=${1:-aeronmd}  # default to 'aeronmd'
 
-for pid in $(pgrep "${pname}"); do
-    echo "PID: ${pid} (${pname})"
-    ps -T -p ${pid} -o spid,comm | while read line; do
-        if [[ ${line} != *"SPID"* ]]; then
-            IFS=" " read -a fields<<<"${line}"
-            aff=$(taskset -cp "${fields[0]}" | tr -d '\n')
-            printf "%s (%s)\n" "${aff}" "${fields[1]}"
-        fi
-    done
-done
+pid=$(pgrep "${pname}")
 
+echo "PID: ${pid} (${pname})"
+ps -T -o pid,tid,comm,psr -p "${pid}"
